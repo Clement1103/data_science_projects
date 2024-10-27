103d08f5dfc9f9b18fa2f37c06776585aa9a4fa1
clement <clement@debian12.home>
2024-10-27 11:16:05 +0100

    Création d'un serveur avec fastapi, serveur définitif généré avec ngrok, fullfilment renseigné

--- DL_projects/chatbot_healthcompany/main.py
+++ DL_projects/chatbot_healthcompany/main.py
@@ -1,1 +1,9 @@
-print('Hello')
+from fastapi import FastAPI
+
+app = FastAPI()
+# uvicorn main:app --reload
+# tunnel non-temporaire ? https://sawfly-wanted-constantly.ngrok-free.app
+@app.get('/')
+
+async def root():
+    return {'message': 'Hello World'}
