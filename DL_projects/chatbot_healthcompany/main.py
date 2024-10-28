d1dc02a154c6d07c2f75366d3d4a36cc20ffac5c
clement <clement@debian12.home>
2024-10-28 12:01:07 +0100

    Fonction pour saisir l'intérêt/le désintérêt écrites, puis vérification des coordonnées données par l'utilisateur.

--- DL_projects/chatbot_healthcompany/main.py
+++ DL_projects/chatbot_healthcompany/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from bdd_helper import *
+from chatbot_helper import *
 
 app = FastAPI()
 # uvicorn main:app --reload
+# ngrok http 8000
 # tunnel non-temporaire ? https://sawfly-wanted-constantly.ngrok-free.app
 @app.post('/')
 
@@ -14,9 +15,13 @@ async def handle_request(request: Request):
     parameters = payload['queryResult']['parameters']
     output_contexts = payload['queryResult']['outputContexts']
 
-    print(f'============{intent}===========')
     if check_if_presentation_product(intent):
         product_tmp = get_product_name(intent)
-        print(product_tmp)
+
+    if intent=='check.coordinates':
+        fulfillmentText = check_coordinates(parameters)
+        return JSONResponse(content={
+            'fulfillmentText': fulfillmentText
+        })
 
     
